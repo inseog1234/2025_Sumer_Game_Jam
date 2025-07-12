4bee48c64d877a833223d05d67dad70512a3277b
inseok <inseog1234@gmail.com>
2025-07-12 11:41:15 +0900

    스테이지 선택, 로딩, 게임 씬 넘어가기

---
+++ Assets/MintCat/Scripts/Share/Loading_Scene.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Loading_Scene : MonoBehaviour
+{
+    public static string nextScene;
+    [SerializeField] Image progressBar;
+
+    private void Start()
+    {
+        nextScene = @"Stage_Scene";
+        StartCoroutine(LoadScene());
+    }
+
+    public static void LoadScene(string sceneName)
+    {
+        nextScene = sceneName;
+        SceneManager.LoadScene("Loading");
+    }
+
+    public IEnumerator LoadScene()
+    {
+        yield return null;
+
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+
+        float fakeProgress = 0f;
+
+        while (fakeProgress < 0.9f)
+        {
+            fakeProgress += Time.deltaTime * 0.5f;
+            progressBar.fillAmount = fakeProgress;
+            yield return null;
+        }
+
+        progressBar.fillAmount = 0.9f;
+
+        while (op.progress < 0.9f)
+        {
+            yield return null;
+        }
+
+        float timer = 0f;
+        while (progressBar.fillAmount < 1.0f)
+        {
+            timer += Time.deltaTime;
+            progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, 1.0f, timer);
+            if (progressBar.fillAmount >= 0.995f)
+            {
+                progressBar.fillAmount = 1.0f;
+                break;
+            }
+            yield return null;
+        }
+
+        op.allowSceneActivation = true;
+    }
+}
---
+++ Assets/MintCat/Scripts/Share/Stage_Manager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage_Manager : MonoBehaviour
+{
+    public int Stage_Num;
+    void Start()
+    {
+        var obj = FindObjectsOfType<Stage_Manager>();
+        if (obj.Length == 1)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void GameStart(int Stage)
+    {
+        Stage_Num = Stage;
+        SceneManager.LoadScene("Loading");
+    }
+
+    void Update()
+    {
+
+    }
+}
