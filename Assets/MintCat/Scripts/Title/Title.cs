3c9f484868e8f81228fc2bc12512e12fd6a4d532
inseok <inseog1234@gmail.com>
2025-07-13 06:18:05 +0900

    타이틀 애니메이션

--- Assets/MintCat/Scripts/Title/Title.cs
+++ Assets/MintCat/Scripts/Title/Title.cs
@@ -12,7 +12,17 @@ public class Title : MonoBehaviour
 
     void Update()
     {
-        
+        Logo_1.GetComponent<RectTransform>().anchoredPosition = new Vector2(Logo_1.GetComponent<RectTransform>().anchoredPosition.x, 180 + Mathf.Cos(Time.deltaTime));
+
+        float pulse = Mathf.Pow(Mathf.Sin(Time.time * 2f), 3f); // 빠르게 up, 천천히 down
+        float scale = 1f + 0.2f * Mathf.Abs(pulse);
+        Logo_2.GetComponent<RectTransform>().localScale = new Vector3(scale, scale, 1f);
+
+
+        Start.GetComponent<RectTransform>().anchoredPosition = new Vector2(Start.GetComponent<RectTransform>().anchoredPosition.x, -208.8f + Mathf.Cos(Time.deltaTime));
+        Setting.GetComponent<RectTransform>().anchoredPosition = new Vector2(Setting.GetComponent<RectTransform>().anchoredPosition.x, -212 + Mathf.Cos(Time.deltaTime));
+        Quit.GetComponent<RectTransform>().anchoredPosition = new Vector2(Quit.GetComponent<RectTransform>().anchoredPosition.x, -211.9f + Mathf.Cos(Time.deltaTime));
+
     }
 
 }
